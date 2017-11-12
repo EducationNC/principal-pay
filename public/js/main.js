1b8779f04957a6ad34a668a0f203035fab6a5f15
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-11-11 21:20:26 -0500

    forgot to prepopulate

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,78 @@ $(document).ready(function(){
   //pym stuff
   var pymChild = new pym.Child();
 
+  //helper functions (FOR THIS FILE ONLY)
   function formatInput(x){
      return "$" + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+
+
+  /***** SALARY CALCULATIONS *****/
+
+    const PERFORMANCE_NAME = {
+      1: "Exceeded",
+      2: "Met",
+      3: "Not Met",
+      4: "Ineligible",
+      5: "Didn't Lead"
+    }
+
+    const ADM_KEY = {
+      1: "0-400",
+      2: "401-700",
+      3: "701-1000",
+      4: "1001-1300",
+      5: "1301+"
+    }
+    const TEACHER_SCORE_TO_PAY_KEY = {
+      "B1":	61751,
+      "B2":	64839,
+      "B3":	67926,
+      "B4":	71014,
+      "B5":	74101,
+      "G1":	67926,
+      "G2":	71322,
+      "G3":	74719,
+      "G4":	78115,
+      "G5":	81511,
+      "E1":	74101,
+      "E2":	77806,
+      "E3":	81511,
+      "E4":	85216,
+      "E5":	88921
+    }
+
+
+  startMeUp()
+
+function startMeUp(){
+  var income_inp_str = ["state_mo", "local_mo", "other_mo"]
+  var incomes = {
+    "state_mo": 0,
+    "local_mo": 0,
+    "other_mo": 0
+  }
+  var perf_inp_str = ["1415", "1516", "1617"]
+  var principal_performance = {
+    1415: 0,
+    1516: 0,
+    1617: 0
+  }
+  var years_exp = $('#years_exp').val();
+  var adm = $('#adm').val();
+
+  /*** INCOME ***/
+  for (i in income_inp_str){
+      incomes[income_inp_str[i]] = $('#' + income_inp_str[i]).val();
+    }
+
+  for (i in perf_inp_str){
+      principal_performance[parseFloat(perf_inp_str[i])] = parseFloat($('#' + perf_inp_str[i]).val());
+    }
+
+  getEstimatedAnnualCompensation(parseFloat(incomes["local_mo"].replace(",","")),
+  parseFloat(incomes["state_mo"].replace(",","")), parseFloat(incomes["other_mo"].replace(",","")), parseFloat(years_exp), principal_performance, adm);
+
 
   //get from the sliders
   $('.output-trigger').change(function(){
@@ -37,6 +106,7 @@ $(document).ready(function(){
     parseFloat(incomes["state_mo"].replace(",","")), parseFloat(incomes["other_mo"].replace(",","")), parseFloat(years_exp), principal_performance, adm);
 
     });
+  }
 
 function updateResults(state, local, other, state_hold_harmless){
 
@@ -49,42 +119,6 @@ function updateResults(state, local, other, state_hold_harmless){
   $('#result_other').html(formatInput(other));
 }
 
-
-
-/***** SALARY CALCULATIONS *****/
-
-  const PERFORMANCE_NAME = {
-    1: "Exceeded",
-    2: "Met",
-    3: "Not Met",
-    4: "Ineligible",
-    5: "Didn't Lead"
-  }
-
-  const ADM_KEY = {
-    1: "0-400",
-    2: "401-700",
-    3: "701-1000",
-    4: "1001-1300",
-    5: "1301+"
-  }
-  const TEACHER_SCORE_TO_PAY_KEY = {
-    "B1":	61751,
-    "B2":	64839,
-    "B3":	67926,
-    "B4":	71014,
-    "B5":	74101,
-    "G1":	67926,
-    "G2":	71322,
-    "G3":	74719,
-    "G4":	78115,
-    "G5":	81511,
-    "E1":	74101,
-    "E2":	77806,
-    "E3":	81511,
-    "E4":	85216,
-    "E5":	88921
-  }
 
   function getEstimatedAnnualCompensation(local_mo, state_mo, other_mo, years_exp, principal_performance, adm_num){
     var performance_letter = getPerformanceLetter(principal_performance);
